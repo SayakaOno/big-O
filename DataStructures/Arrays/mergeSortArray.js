e0f3d4bfbf0eb432cd087769a364d05dc3cde290
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-03-12 16:48:07 -0700

    add mergeSortArray.js

---
+++ DataStructures/Arrays/mergeSortArray.js
@@ -0,0 +1,42 @@
+function mergeSortedArrays1(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+
+  return mergedArray;
+}
+
+function mergeSortedArrays2(arr1, arr2) {
+  if (arr1.length === 0) {
+    return arr2;
+  }
+  if (arr2.length === 0) {
+    return arr1;
+  }
+
+  let mergedArray = [...arr1, ...arr2];
+  return mergedArray.sort((a, b) => a - b);
+}
+
+console.log(mergeSortedArrays1([0, 3, 4, 31], [4, 6, 30]));
