fa96a8fc538be8e0acf397f8c47627b228cf2f8f
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-03-23 15:00:00 -0700

    add breadthFirstSearch methods to BinarySearchTree.js

--- DataStructures/Trees/BinarySearchTree.js
+++ DataStructures/Trees/BinarySearchTree.js
@@ -54,6 +54,40 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  breadthFirstSearch() {
+    let currentNodes = [this.root];
+    let list = [];
+
+    while (currentNodes.length) {
+      list.push(currentNodes[0].value);
+      if (currentNodes[0].left) {
+        currentNodes.push(currentNodes[0].left);
+      }
+      if (currentNodes[0].right) {
+        currentNodes.push(currentNodes[0].right);
+      }
+      currentNodes.shift();
+    }
+    console.log(list);
+    return list;
+  }
+
+  breadthFirstSearchR(currentNodes, list) {
+    if (!currentNodes.length) {
+      console.log(list);
+      return list;
+    }
+    list.push(currentNodes[0].value);
+    if (currentNodes[0].left) {
+      currentNodes.push(currentNodes[0].left);
+    }
+    if (currentNodes[0].right) {
+      currentNodes.push(currentNodes[0].right);
+    }
+    currentNodes.shift();
+    return this.breadthFirstSearchR(currentNodes, list);
+  }
 }
 
 function traverse(node) {
@@ -62,3 +96,14 @@ function traverse(node) {
   tree.rigth = node.right === null ? null : traverse(node.right);
   return tree;
 }
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+tree.breadthFirstSearchR([tree.root], []);
